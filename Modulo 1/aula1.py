6fe4bc4e3510e974de4a620a90c50e4ce361d86f
Alessa Santos <alessa.santos@vericode.com.br>
2022-12-05 20:27:49 -0300

    Primeiro commit

---
+++ Modulo 1/aula1.py
@@ -0,0 +1,36 @@
+# Referências utilizadas sobre python e vcscode
+# https://www.python.org
+# https://python.org.br
+# Documentação oficial: https://docs.python.org/pt-br/3/tutorial/
+# Playlist python: https://www.youtube.com/watch?v=cDqMbI02hRs&list=PLcmVV8telDGzH6wFY_9h_KZ3Sn7EfjT1D
+# https://www.blog.duomly.com/tags/python/
+
+'''
+isso comenta um bloco de comentario
+posso continuar comentando na linha de baixo 
+por exemplo
+'''
+'''
+Configurar atalho para rodar o python: configurações > teclas de atalho >> *busca o pyrthon:run python*
+'''
+
+
+## 1 - Sintaxe em python 
+
+
+## Vamos começar imprimindo "Hello World!" na sáida padrão
+## Para imprimir utilizamos a função print(), perceba que para utilizar funções 
+## usamos os parenteses(). E dentro dos parenteses passamos o que a função precisa ser executada. 
+
+## O caso de uso mais simples do print é o seguinte: 
+
+print ("Hello World!")
+
+## Também podemos rodar a função print usando o argumento com aspas simples '
+print ('Hello World')
+
+## Posso chamar a função print sem nenhum argumento. Isso imprime uma linha vazia.
+print()
+
+## Note que não precisamos de ; entre cada comando. 
+
